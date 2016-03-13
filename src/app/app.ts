10565a1a5099e8dc2107a041c6ac1bea5e93f34f
Kapil Sachdeva <ksachdeva17@gmail.com>
2016-03-12 19:25:38 -0600

    refactor - move the example from app.ts/html to example1.ts/html so that we can add more examples

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,65 +1,16 @@
-import {Component, ViewChild, AfterViewInit} from 'angular2/core';
-import {KSSwiperContainer, KSSwiperSlide} from './components/ks-swiper';
+import {Component} from 'angular2/core';
+import {Example1} from './example1/example1';
+
+require('./app.css');
 
 @Component({
   selector: 'app',
   pipes: [],
   providers: [],
-  directives: [KSSwiperContainer, KSSwiperSlide],
-  styles: [`
-    .myslides {
-      background: #eee;
-      font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
-      font-size: 14px;
-      color: #000;
-      margin: 0;
-      padding: 0;
-      height: 100px;
-      border-bottom: 1px solid gray;
-    }
-
-    .swiper-container {
-      width: 100%;
-      height: 100%;
-    }
-
-    .swiper-slide {
-      text-align: center;
-      font-size: 18px;
-      background: #fff;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-  `],
+  directives: [Example1],
   template: require('./app.html')
 })
-export class App implements AfterViewInit {
-
-  // this is how you get access to the child component
-  @ViewChild(KSSwiperContainer) swiperContainer: KSSwiperContainer;
-
-  example1SwipeOptions: any;
-
+export class App {
   constructor() {
-    this.example1SwipeOptions = {
-      slidesPerView: 4,
-      loop: false,
-      spaceBetween: 5
-    };
   }
-
-  moveNext() {
-    this.swiperContainer.swiper.slideNext();
-  }
-
-  movePrev() {
-    this.swiperContainer.swiper.slidePrev();
-  }
-
-  ngAfterViewInit() {
-    console.log('here');
-    console.log(this.swiperContainer);
-  }
-
 }
---
+++ src/app/example1/example1.ts
@@ -0,0 +1,38 @@
+import {Component, ViewChild, AfterViewInit} from 'angular2/core';
+import {KSSwiperContainer, KSSwiperSlide} from './../components/ks-swiper';
+
+@Component({
+  selector: 'example1',
+  pipes: [],
+  providers: [],
+  directives: [KSSwiperContainer, KSSwiperSlide],
+  template: require('./example1.html')
+})
+export class Example1 implements AfterViewInit {
+
+  // this is how you get access to the child component
+  @ViewChild(KSSwiperContainer) swiperContainer: KSSwiperContainer;
+
+  example1SwipeOptions: any;
+
+  constructor() {
+    this.example1SwipeOptions = {
+      slidesPerView: 4,
+      loop: false,
+      spaceBetween: 5
+    };
+  }
+
+  moveNext() {
+    this.swiperContainer.swiper.slideNext();
+  }
+
+  movePrev() {
+    this.swiperContainer.swiper.slidePrev();
+  }
+
+  ngAfterViewInit() {
+    console.log(this.swiperContainer);
+  }
+
+}
